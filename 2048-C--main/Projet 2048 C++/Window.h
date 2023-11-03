397598665ca74f8b0349ebfa106e0b8bd9ace190
lilian516 <115982783+lilian516@users.noreply.github.com>
2023-11-03 10:29:33 +0100

    Add files via upload

---
+++ 2048-C--main/Projet 2048 C++/Window.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Grid.h";
+#include <SDL.h>
+class Window {
+public:
+
+    SDL_Renderer* m_renderer;
+    SDL_Window* m_window;
+    
+    Window();
+
+    void display();
+    void displayGrid(vector<int> arrayValue);
+    void clear();
+};
+
+
+
